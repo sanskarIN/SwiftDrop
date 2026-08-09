f3509230242a33db97512450915c2c07c9c47efa
Sanskar <sanskarin@outlook.in>
2026-08-09 11:54:24 +0530

    security: use modern certificate loader for generated identity
    Signed-off-by: Sanskar <sanskarin@outlook.in>

--- src/SwiftDrop.Core/Security/CertificateService.cs
+++ src/SwiftDrop.Core/Security/CertificateService.cs
@@ -7,12 +7,21 @@ public sealed class CertificateService
 {
     public X509Certificate2 CreateSelfSigned(string deviceId)
     {
+        ArgumentException.ThrowIfNullOrWhiteSpace(deviceId);
         using var key = ECDsa.Create(ECCurve.NamedCurves.nistP256);
         var request = new CertificateRequest($"CN=SwiftDrop-{deviceId}", key, HashAlgorithmName.SHA256);
         request.CertificateExtensions.Add(new X509BasicConstraintsExtension(false, false, 0, true));
         request.CertificateExtensions.Add(new X509KeyUsageExtension(X509KeyUsageFlags.DigitalSignature, true));
         request.CertificateExtensions.Add(new X509SubjectKeyIdentifierExtension(request.PublicKey, false));
-        var cert = request.CreateSelfSigned(DateTimeOffset.UtcNow.AddMinutes(-5), DateTimeOffset.UtcNow.AddYears(5));
-        return new X509Certificate2(cert.Export(X509ContentType.Pfx));
+        using var certificate = request.CreateSelfSigned(DateTimeOffset.UtcNow.AddMinutes(-5), DateTimeOffset.UtcNow.AddYears(5));
+        var pfx = certificate.Export(X509ContentType.Pfx);
+        try
+        {
+            return X509CertificateLoader.LoadPkcs12(pfx, null);
+        }
+        finally
+        {
+            CryptographicOperations.ZeroMemory(pfx);
+        }
     }
 }
