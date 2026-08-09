8f327b84d1db1f2759f1370d2402bcb1e2b0c22c
Sanskar <sanskarin@outlook.in>
2026-08-09 14:06:08 +0530

    fix: migrate MAUI startup to CreateWindow lifecycle
    Signed-off-by: Sanskar <sanskarin@outlook.in>

--- src/SwiftDrop.App/App.xaml.cs
+++ src/SwiftDrop.App/App.xaml.cs
@@ -4,13 +4,42 @@ namespace SwiftDrop.App;
 
 public partial class App : Application
 {
+    private readonly MainPage _mainPage;
+    private readonly EventHandler _externalInputChanged;
+
     public App(AppSettingsService settings, AppearanceService appearance, MainPage page)
     {
         InitializeComponent();
         appearance.Apply(settings.Load());
         ExternalInputInbox.PruneStagedCache(TimeSpan.FromHours(24));
-        MainPage = new NavigationPage(page);
-        ExternalInputInbox.Changed += async (_, _) => await page.ApplyExternalInputAsync();
-        MainThread.BeginInvokeOnMainThread(async () => await page.ApplyExternalInputAsync());
+        _mainPage = page;
+        _externalInputChanged = OnExternalInputChanged;
+        ExternalInputInbox.Changed += _externalInputChanged;
+    }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        var window = new Window(new NavigationPage(_mainPage));
+        window.Destroying += OnWindowDestroying;
+        MainThread.BeginInvokeOnMainThread(async () => await _mainPage.ApplyExternalInputAsync());
+        return window;
+    }
+
+    private async void OnExternalInputChanged(object? sender, EventArgs e)
+    {
+        try
+        {
+            await _mainPage.ApplyExternalInputAsync();
+        }
+        catch
+        {
+        }
+    }
+
+    private async void OnWindowDestroying(object? sender, EventArgs e)
+    {
+        ExternalInputInbox.Changed -= _externalInputChanged;
+        if (sender is Window window) window.Destroying -= OnWindowDestroying;
+        await _mainPage.DisposeAsync();
     }
 }
