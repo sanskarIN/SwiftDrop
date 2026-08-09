bca2f42460c433c762b74860da3879884408d78e
Sanskar <sanskarin@outlook.in>
2026-08-09 12:50:51 +0530

    feat: handle SwiftDrop pairing deep links on Mac Catalyst
    Signed-off-by: Sanskar <sanskarin@outlook.in>

--- src/SwiftDrop.App/Platforms/MacCatalyst/AppDelegate.cs
+++ src/SwiftDrop.App/Platforms/MacCatalyst/AppDelegate.cs
@@ -1,4 +1,22 @@
 using Foundation;
+using SwiftDrop.App.Services;
+using UIKit;
+
 namespace SwiftDrop.App;
+
 [Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate { protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp(); }
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+    public override bool OpenUrl(UIApplication application, NSUrl url, NSDictionary options)
+    {
+        var link = url.AbsoluteString;
+        if (!string.IsNullOrWhiteSpace(link) && link.StartsWith("swiftdrop://pair", StringComparison.OrdinalIgnoreCase))
+        {
+            ExternalInputInbox.SetPairingLink(link);
+            return true;
+        }
+        return base.OpenUrl(application, url, options);
+    }
+}
