23203f6388a53120b6ca438fe5c603b1b4fc8f1b
Sanskar <sanskarin@outlook.in>
2026-08-09 11:56:07 +0530

    feat: restore saved app theme at startup
    Signed-off-by: Sanskar <sanskarin@outlook.in>

--- src/SwiftDrop.App/App.xaml.cs
+++ src/SwiftDrop.App/App.xaml.cs
@@ -1,10 +1,19 @@
+using SwiftDrop.App.Services;
+
 namespace SwiftDrop.App;
 
 public partial class App : Application
 {
-    public App(MainPage page)
+    public App(AppSettingsService settings, MainPage page)
     {
         InitializeComponent();
+        var saved = settings.Load();
+        UserAppTheme = saved.Theme switch
+        {
+            "Light" => AppTheme.Light,
+            "Dark" => AppTheme.Dark,
+            _ => AppTheme.Unspecified
+        };
         MainPage = new NavigationPage(page);
     }
 }
