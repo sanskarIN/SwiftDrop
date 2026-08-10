c95076de6f2b06e6b3afdfd8c86eeaf0542f1643
Sanskar <sanskarin@outlook.in>
2026-08-10 16:31:31 +0530

    feat: import Apple share packages on launch and activation
    Signed-off-by: Sanskar <sanskarin@outlook.in>

--- src/SwiftDrop.App/App.xaml.cs
+++ src/SwiftDrop.App/App.xaml.cs
@@ -24,16 +24,27 @@ public partial class App : Application
     protected override Window CreateWindow(IActivationState? activationState)
     {
         var window = new Window(new NavigationPage(_mainPage));
+        window.Activated += OnWindowActivated;
         window.Destroying += OnWindowDestroying;
         MainThread.BeginInvokeOnMainThread(async () =>
         {
+            await ImportAppleSharesBestEffortAsync();
             await _mainPage.ShowIdentityRecoveryNoticeAsync();
             await _mainPage.ApplyExternalInputAsync();
         });
         return window;
     }
 
+    private async void OnWindowActivated(object? sender, EventArgs e)
+    {
+        await ImportAppleSharesBestEffortAsync();
+        await ApplyExternalInputBestEffortAsync();
+    }
+
     private async void OnExternalInputChanged(object? sender, EventArgs e)
+        => await ApplyExternalInputBestEffortAsync();
+
+    private async Task ApplyExternalInputBestEffortAsync()
     {
         try
         {
@@ -44,10 +55,29 @@ public partial class App : Application
         }
     }
 
+    private static async Task ImportAppleSharesBestEffortAsync()
+    {
+#if IOS || MACCATALYST
+        try
+        {
+            await AppleShareContainerImporter.ImportPendingAsync();
+        }
+        catch
+        {
+        }
+#else
+        await Task.CompletedTask;
+#endif
+    }
+
     private async void OnWindowDestroying(object? sender, EventArgs e)
     {
         ExternalInputInbox.Changed -= _externalInputChanged;
-        if (sender is Window window) window.Destroying -= OnWindowDestroying;
+        if (sender is Window window)
+        {
+            window.Activated -= OnWindowActivated;
+            window.Destroying -= OnWindowDestroying;
+        }
         await _mainPage.DisposeAsync();
     }
 }
